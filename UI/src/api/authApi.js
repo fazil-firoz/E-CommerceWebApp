5e83a216c15dea162674c99ebf23ba3cfcd91710
Mohammed Fazil Firoz <mohammedfazilfirozmkv@gmail.com>
2026-07-19 23:37:15 +0530

    s

--- UI/src/api/authApi.js
+++
@@ -1,7 +0,0 @@
-import apiClient from './api';
-import { URLS } from '../config/urlConfig';
-
-export const authApi = {
-  sendOtp: (email) => apiClient.post(URLS.AUTH.SEND_OTP, { email }),
-  verifyOtp: (email, otp) => apiClient.post(URLS.AUTH.VERIFY_OTP, { email, otp }),
-};
